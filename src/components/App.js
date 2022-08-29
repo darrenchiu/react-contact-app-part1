63800ee3fc60fb700341688ee47c3ca6b2f45463
Darren Chiu <chiusw2003@gmail.com>
2022-08-29 17:03:42 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import uuid from 'react-uuid';
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([])
+
+  const addContactHandler = (contact) => {
+    console.log(contact)
+    setContacts([...contacts, {id: uuid(), ...contact}])
+  }
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact)=> {
+      return contact.id !== id;
+    })
+
+    setContacts(newContactList);
+  }
+
+  useEffect(() => {
+    console.log("Hello")
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log(retrieveContacts)
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts])
+
+  // const contacts = [
+  //   {
+  //     id: "1",
+  //     "name": "Dipesh",
+  //     email: "malvia@gmail.com"
+  //   },
+  //   {
+  //     id: "2",
+  //     "name": "Nikesh",
+  //     email: "nicks@gmail.com"
+  //   },
+  // ]
+
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList  contacts={contacts} getContactId={removeContactHandler}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ContactCard from "./ContactCard";
+
+
+
+const ContactList = (props) => {
+    console.log(props)
+
+    const removeContactHandler = (id) => {
+        props.getContactId(id)
+    }
+
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+            <ContactCard contact={contact} clickHandler={removeContactHandler} key={contact.id}></ContactCard>
+        )
+    });
+
+    return (
+        <div className="ui celled list">
+            {renderContactList}
+        </div>
+    )
+}
+
+export default ContactList;
